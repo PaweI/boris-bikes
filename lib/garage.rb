dbd2c964d98e7d8787f385af15f65aacc0ef4a83
Pavel Redics <pavel.redics@gmail.com>
2014-10-30 22:38:03 +0000

    added garage spec's and code

---
+++ lib/garage.rb
@@ -0,0 +1,29 @@
+class Garage 
+
+attr_accessor :bikes
+
+	def initialize
+		@bikes = []
+	end
+
+	def dock(bike)
+		@bikes << bike
+	end
+
+
+	def repair(bike)
+		bike.fix!
+	end
+
+
+	def transfer_to(van)
+		@bikes.each do |bike|
+			bikes.delete(bike) if !bike.broken?
+			van.dock(bike) if !bike.broken?
+		end
+	end
+
+
+
+
+end
---
+++ spec/garage_spec.rb
@@ -0,0 +1,30 @@
+require 'garage'
+
+describe Garage do 
+	let(:garage)		{Garage.new}
+	let(:bike)			{double :bike, broken?: false}
+	let(:bike_broken)	{double :bike, broken?: true}
+	let(:van)			{double :van}
+
+	it 'will receive broken bikes' do
+		expect(garage.bikes.count).to eq(0)
+		garage.dock(bike_broken)
+		expect(garage.bikes.count).to eq(1)
+	end
+
+	it 'will fix broken bikes' do
+		garage.dock(bike_broken)
+		expect(bike_broken).to receive(:fix!)
+		garage.repair(bike_broken)
+		
+	end 
+
+	it 'will release fixed bikes to the van' do
+		garage.dock(bike)
+		expect(van).to receive(:dock).with(bike)
+		garage.transfer_to(van)
+
+	end
+
+
+end
